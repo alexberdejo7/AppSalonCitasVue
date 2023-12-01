a9d7fbb4b0026f2e5838c9b5eb643f45810ea7a4
alexandro.berdejo <alexandro.berdejo@gmail.com>
2023-12-01 01:48:05 -0600

    'ConnectBackend,ServiceItems,storeService'

---
+++ src/components/ServicesItem.vue
@@ -0,0 +1,25 @@
+
+<script setup>
+import { formatCurrency} from '../helpers/formatter'
+
+defineProps({
+  service: {
+    type: Object
+  } 
+})
+
+
+
+</script>
+
+
+<template>
+
+  <div
+    class="p-5 space-y-5 rounded-lg cursor-pointer bg-sky-200"
+  >
+      <p class="text-2xl font-light"> {{service.name}}</p>
+
+      <p class="text-lg " > {{formatCurrency( service.price)}} </p>
+  </div>
+</template>
--- src/views/appointments/ServicesView.vue
+++ src/views/appointments/ServicesView.vue
@@ -1,13 +1,26 @@
 
 <script setup>
+import ServicesItem from '../../components/ServicesItem.vue';
+import {useServiceStore} from '../../stores/services'
 
+const storeservice = useServiceStore()
 
 </script>
 
 
 
 <template>
-  <div>
-<h1> desde sercios</h1>
+  
+  <h2 class="text-4xl font-extrabold text-white mt-10"> Servicios </h2>
+  <p class="text-white text-lg mt-4"> A continuacion elige al menus un servicio</p>
+    
+  <div class="grid grid-cols-2 gap-5 mt-5">
+    <ServicesItem 
+      v-for="service in storeservice.services"
+      :key="service._id"
+      :service="service"
+
+    />
+    
   </div>
 </template>
