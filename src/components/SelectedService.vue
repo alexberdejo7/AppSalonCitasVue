a86f6f4a04addfb94b4677cf84a1d9096ab9017a
alexandro.berdejo <alexandro.berdejo@gmail.com>
2023-12-01 22:01:58 -0600

    'StoreAppointmentjs,TotalAccount,ShowingServices'

---
+++ src/components/SelectedService.vue
@@ -0,0 +1,35 @@
+
+<script setup>
+import { formatCurrency } from '../helpers/formatter';
+import { useAppointmentsStore } from '../stores/appointmens';
+
+const appointments = useAppointmentsStore()
+
+
+defineProps({
+  service: {
+    type: Object
+  }
+})
+
+
+</script>
+
+<template>
+  <div class="border border-sky-600 rounded-lg p-4 flex justify-between items-center">
+      <div>
+        <p class="text-xl font-extrabold text-white"> {{ service.name }}</p>
+        <p class="text-xl font-normal text-amber-400"> {{ formatCurrency (service.price) }}</p>
+      </div>
+      
+      
+      <button
+        type="button"
+        class="inlinex-flex items-center rounded-lg bg-red-500 text-white text-sm px-3 py-2 font-semibold shadow-sm ring-1 ring-inset ring-red-600 hover:bg-red-700"
+        @click="appointments.onServicesSelected(service)"
+        > Eliminar
+      </button>
+  
+</div>
+
+</template>
--- src/views/appointments/AppointmentView.vue
+++ src/views/appointments/AppointmentView.vue
@@ -1,14 +1,41 @@
 
 <script setup>
+import SelectedService from '../../components/SelectedService.vue';
+import { formatCurrency } from '../../helpers/formatter';
+import { useAppointmentsStore } from '../../stores/appointmens';
+
+const appointments = useAppointmentsStore()
 
 </script>
 
-
-
-
 <template>
-  <div>
-    <h1> detalles vcita</h1>
+
+  <h2 class="text-3xl font-extrabold text-white mt-2"> Detalles y Resumen de tu cita </h2>
+  <p class="text-white text-md"> A continuacion verifica tu cita </p>
+
+  <h3 class="text-xl font-semibold mt-5 text-white"> Servicios  </h3>
+
+  <!-- Computed si no se ha seleccionado servicios  -->
+    <p 
+        class="text-white text-3xl text-center"
+        v-if="appointments.noServiceSelected" 
+    >  No has seleccionado Servicios </p>
+
+
+  <div v-else  class="grid gap-3">
+      <SelectedService
+          v-for="service in appointments.services"
+          :key="service._id"
+          :service="service"
+      />
+
+    <p 
+        class="text-right text-amber-400 text-xl"
+    > Total: <span class="font-bold"> {{ formatCurrency(appointments.totalAccount) }}
+     </span> 
+    </p>
+
 
   </div>
+
 </template>
