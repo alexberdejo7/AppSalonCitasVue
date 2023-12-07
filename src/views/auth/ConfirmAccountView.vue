52aeb1756af04bcd816b012bd8ee553bffcbe495
alexandro.berdejo <alexandro.berdejo@gmail.com>
2023-12-07 00:17:20 -0600

    'AuthApiVerifyLoginConfirmAccount'

--- src/views/auth/ConfirmAccountView.vue
+++ src/views/auth/ConfirmAccountView.vue
@@ -1,14 +1,41 @@
 <script setup>
+import { onMounted, inject } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import AuthApi from '../../api/AuthApi'
+
+
+
+const toast = inject('toast')
+
+const router = useRouter()
+const route = useRoute()
+const { token } = route.params
+
+onMounted(async () => {
+    try {
+      const { data } = await AuthApi.verifyAccount(token)
+      toast.open({
+        message: data.msg,
+        type: 'success'
+      })
+      setTimeout(() => {
+        router.push({name: 'login'})
+      }, 4000)
+
+    } catch (error) {
+      toast.open({
+        message: error.response.data.msg,
+        type: 'error'
+      })
+    }
+
+})
 
 
 
 </script>
 
+
 <template>
-  <div>
-    <h2>Confirmar acsadscc</h2>
-
-    
-
-  </div>
+ <h1 class="text-5xl font-extrabold text-white text-center mt-7"> Verifica tu cuenta </h1>
 </template>
--- src/views/auth/LogintView.vue
+++ src/views/auth/LogintView.vue
@@ -1,14 +1,69 @@
 <script setup>
+import AuthApi from '../../api/AuthApi.js'
+import {inject} from 'vue'
 
+const toast = inject('toast')
 
+const handleSubmit = async(formData) => {
+    try {
+        const {data} = await AuthApi.login(formData)
+        console.log(data)
+    }
+    catch (error) {
+        toast.open({
+          message: error.response.data.msg,
+          type: 'error'
+        })
+      }
+    }
+    
 
 </script>
 
 <template>
-  <div>
-    <h2>Login</h2>
+  <h1 class="text-5xl font-extrabold text-white text-center mt-7"> Inicia sesión </h1>
+  <p class="text-md italic text-white text-center my-5"> Si ya tienes una cuenta inicia sesion.  Si no ve a crear cuenta  </p>
 
-    
+      <FormKit
+        id="registerForm"
+        type="form"
+        :actions="false"
+        incomplete-message="Ups! Error al crear tu cuenta, revisa las notificaciones"
+        @submit="handleSubmit"
+        > 
 
-  </div>
+          <FormKit
+            type="email"
+            label="Email"
+            name="email"
+            placeholder="Tu correo de usuario"
+            validation="required|email"
+            :validation-messages="{
+              required: 'El correo es obligatorio',
+              email: 'El correo es invalido'
+            }"
+          >
+        </FormKit>
+
+        <FormKit
+            type="password"
+            label="Password"
+            name="password"
+            placeholder="Contraseña de usuario"
+            validation="required"
+            :validation-messages="{
+              required: 'La contraseña es obligatorio',
+            }"
+          >
+        </FormKit>
+
+
+
+
+
+
+        <FormKit type="submit"> INICIAR SESION </FormKit>
+
+
+      </FormKit>
 </template>
