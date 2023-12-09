2ed08adb1e2a035ff51f82c7af845866248f2283
alexandro.berdejo <alexandro.berdejo@gmail.com>
2023-12-09 03:17:38 -0600

    'respaldo08dec'

--- src/views/appointments/MyAppointmentsView.vue
+++ src/views/appointments/MyAppointmentsView.vue
@@ -7,7 +7,7 @@
 
 <template>
   <h2 class="text-4xl font-extrabold text-white mt10"> Mis citas</h2>
-  <p class="text-white text-lg mt-5"> Administra tus citas </p>
+  <p class="text-white text-lg mt-5"> Administra tus citas aqui: </p>
 
 
 </template>
